364dc4890ae0a69b4ce35c73f89b354b285bd526
martijnkunstman <martijn@kunstman.nl>
2021-11-26 22:03:10 +0100

    no canvas visual

--- public/script.js
+++ public/script.js
@@ -2,36 +2,25 @@ const startRecording = document.querySelector('#startRecording');
 const stopRecording = document.querySelector('#stopRecording');
 const playRecorded = document.querySelector('#playRecorded');
 const stopRecorded = document.querySelector('#stopRecorded');
-
-const canvas = document.querySelector('.visualizer');
-const canvasCtx = canvas.getContext("2d");
 const pitchInput = document.querySelector('#pitch');
 const pitchLabel = document.querySelector('#pitchLabel');
+const constraints = { audio: true };
 
-startRecording.addEventListener("click", async () => {
-    await Tone.start();
-    console.log("context started");
-});
+let pitchShift;
+let player;
+let chunks = [];
 
 pitchInput.addEventListener('input', function () {
     pitchLabel.innerHTML = pitchInput.value;
 }, false);
 
-let audioCtx;
-let pitchShift;
-let player;
-
 if (navigator.mediaDevices.getUserMedia) {
-
-    const constraints = { audio: true };
-    let chunks = [];
 
     let onSuccess = function (stream) {
         const mediaRecorder = new MediaRecorder(stream);
 
-        visualize(stream);
-
-        startRecording.addEventListener('click', function () {
+        startRecording.addEventListener('click', async () => {
+            await Tone.start();
             startRecording.disabled = true;
             stopRecording.disabled = false;
             mediaRecorder.start();
@@ -53,7 +42,7 @@ if (navigator.mediaDevices.getUserMedia) {
         stopRecorded.addEventListener('click', function () {
             stopRecorded.disabled = true;
             startRecording.disabled = false;
-            player.stop();           
+            player.stop();
         });
 
         mediaRecorder.onstop = function (e) {
@@ -78,60 +67,4 @@ if (navigator.mediaDevices.getUserMedia) {
 
 } else {
     console.log('getUserMedia not supported on your browser!');
-}
-
-function visualize(stream) {
-    if (!audioCtx) {
-        audioCtx = new AudioContext();
-    }
-
-    const source = audioCtx.createMediaStreamSource(stream);
-
-    const analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 2048;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    source.connect(analyser);
-
-    draw()
-
-    function draw() {
-        const WIDTH = canvas.width
-        const HEIGHT = canvas.height;
-
-        requestAnimationFrame(draw);
-
-        analyser.getByteTimeDomainData(dataArray);
-
-        canvasCtx.fillStyle = 'rgba(200, 200, 200, 0.05)';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-        canvasCtx.lineWidth = 2;
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-        canvasCtx.beginPath();
-
-        let sliceWidth = WIDTH * 1.0 / bufferLength;
-        let x = 0;
-
-
-        for (let i = 0; i < bufferLength; i++) {
-
-            let v = dataArray[i] / 128.0;
-            let y = v * HEIGHT / 2;
-
-            if (i === 0) {
-                canvasCtx.moveTo(x, y);
-            } else {
-                canvasCtx.lineTo(x, y);
-            }
-
-            x += sliceWidth;
-        }
-
-        canvasCtx.lineTo(canvas.width, canvas.height / 2);
-        canvasCtx.stroke();
-
-    }
 }
